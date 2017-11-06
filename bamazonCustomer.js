f89ec044fae1ce3515aa62d28cc72216b3a7ba3d
Felix Moon <daekeun.moon@gmail.com>
2017-11-05 22:40:08 -0600

    Displayed item list using cli-table package

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+var Table = require("cli-table");
 
 var connection = mysql.createConnection({
 	host: "localhost",
@@ -17,7 +18,22 @@ connection.connect(function(err) {
 
 	// console.log("connected as id " + connection.threadId + "\n");
 
-	processOrder();
+	connection.query("SELECT * FROM products", function(err, res) {
+		if (err) throw err;
+
+		var table = new Table({
+			head: ['item_id', 'product_name', 'department_name', 'price', 'stock_quantity'],
+			colWidths: [10, 40, 20, 10, 18]
+		});
+		 
+		for(var i=0; i<res.length; i++) {
+			table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+		}
+
+		console.log(table.toString());
+
+		processOrder();
+	});
 });
 
 function processOrder() {
